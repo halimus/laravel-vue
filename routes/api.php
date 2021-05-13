452645bb0714be994372b4c01f72a6ae1305bca0
Halim <alardjane@readyworks.com>
2021-05-12 23:20:02 -0400

    create routes, controllers, resources

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('categories', [CategoryController::class, 'index'])->name('categories');
+Route::get('products', [ProductController::class, 'index'])->name('products');
